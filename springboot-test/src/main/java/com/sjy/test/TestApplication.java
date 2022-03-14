398582e8f4fd8b26a861ec50777c58d8dee392b9
shijingyang <1330037178@qq.com>
2022-03-14 21:48:31 +0800

    update

--- springboot-test/src/main/java/com/sjy/test/TestApplication.java
+++ springboot-test/src/main/java/com/sjy/test/TestApplication.java
@@ -1,18 +1,11 @@
 package com.sjy.test;
 
-import ch.qos.logback.classic.net.SimpleSocketServer;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.annotation.JSONField;
-import com.sjy.common.utils.config.RedisService;
-import org.apache.logging.log4j.LogManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Scope;
+
+import java.util.concurrent.locks.ReentrantLock;
 
 
 public class TestApplication {
@@ -20,6 +13,14 @@ public class TestApplication {
     public static void main(String[] args) throws Exception {
         User user = new User("id","2s");
         System.out.println(JSONObject.toJSONString(user));
+        ReentrantLock lock = new ReentrantLock();
+        lock.lock();
+        try {
+        }catch (Exception e){
+
+        }finally {
+            lock.unlock();
+        }
     }
 
 }
---
+++ wiki/java/基础/锁/无锁CAS-AQS.md
@@ -0,0 +1,11 @@
+### CAS
+
+// var1 要修改的值
+// var2 期望的值
+// var3 修改的值
+compareAndSwap(var var1 ,int  var1 ,int var2)
+比较并交换，
+cas 是通过CPU实现的，不用的操作系统对应的方法名称不一样
+
+### AQS
+AbstractQueuedSynchronizer  
